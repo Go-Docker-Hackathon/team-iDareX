b3cdd5b20193e30e467b6dd20f1d5624fee519a5
langshuang <409909041@qq.com>
2015-06-06 16:54:32 +0800

    添加了七牛地址常量

--- vendor/upload/qiniu/upload.go
+++ vendor/upload/qiniu/upload.go
@@ -2,20 +2,21 @@ package qiniu
 
 import (
 	"github.com/qiniu/api/rs"
-	"github.com/Go-Docker-Hackathon/team-iDareX/vendor/db/mongo"
-	"labix.org/v2/mgo/bson"
-	"fmt"
 )
 
 import . "github.com/qiniu/api/conf"
 
+const DOMAIN = "http://7xjj3q.com1.z0.glb.clouddn.com/"
+
 func UploadQiniu(videoUrl string) (key string, err error) {
-	ACCESS_KEY = "A3opmNKS8XPKzt5ks5C2um__tYL2E6dZu81Xjzim"
-	SECRET_KEY = "SCaNo1SxnFnjyxOEJm08OBkCIhA0R7HOHOmIMAZU"
+
+	ACCESS_KEY = "WoXW_P0kS1TVQ_eXVcs-gRfgxoYUGHFs-wQ9OPKw"
+	SECRET_KEY = "X5pHXl9qB079ZX8ppeHTslUkZfqTLm3qhUdac63z"
 	BUCKET_NAME := "youtubetmp"
-	
+
 	uploadToken := getUploadToken(BUCKET_NAME)
 	key, err = uploadFile(videoUrl, uploadToken)
+
 	return
 }
 
