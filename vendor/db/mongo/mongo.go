7e2cd8a9d63944b61569ba00352c95432226dfcd
maxwelldu <dcj3sjt@163.com>
2015-06-06 14:12:25 +0800

    adjust mongodb connection info

--- vendor/db/mongo/mongo.go
+++ vendor/db/mongo/mongo.go
@@ -3,6 +3,7 @@ package mongo
 import (
 	"labix.org/v2/mgo"
 	"os"
+	"fmt"
 )
 
 var Session *mgo.Session
@@ -10,27 +11,36 @@ var Collection *mgo.Collection
 
 func Connect() *mgo.Collection{
 
-MONGODB_PORT
-MONGODB_PORT_27017_TCP_ADDR
 
-MONGODB_PORT_27017_TCP_PROTO
-MONGODB_PORT_27017_TCP_PORT
-
-
-
-
-	MONGODB := os.Getenv("MongoDB")
 	MONGODB_USERNAME := os.Getenv("MONGODB_USERNAME")
 	MONGODB_PASSWORD := os.Getenv("MONGODB_PASSWORD")
-	MONGODB_PORT_27017_TCP := os.Getenv("MONGODB_PORT_27017_TCP")	
+	MONGODB_PORT_27017_TCP_ADDR := os.Getenv("MONGODB_PORT_27017_TCP_ADDR")	
+	MONGODB_PORT_27017_TCP_PORT := os.Getenv("MONGODB_PORT_27017_TCP_PORT")	
 	MONGODB_INSTANCE_NAME := os.Getenv("MONGODB_INSTANCE_NAME")
 
-//	mongodb://myuser:mypass@localhost:40001/mydb	
-	Session, _ = mgo.Dial(MONGODB+"://" + MONGODB_USERNAME + ":" + MONGODB_PASSWORD + "@(" + MONGODB_PORT_27017_TCP+")/" + MONGODB_INSTANCE_NAME)
+	fmt.Println("username "+ MONGODB_USERNAME)
 	
+	if MONGODB_USERNAME != "" {
+		Session, _ = mgo.Dial("mongodb://" + MONGODB_USERNAME + ":" + MONGODB_PASSWORD + "@" + MONGODB_PORT_27017_TCP_ADDR + ":"  + MONGODB_PORT_27017_TCP_PORT + "/" + MONGODB_INSTANCE_NAME)		
+	} else {
+		fmt.Println("username "+ MONGODB_USERNAME)
+		
+		if MONGODB_PORT_27017_TCP_ADDR != ""  {
+			Session, _ = mgo.Dial("mongodb://" + MONGODB_PORT_27017_TCP_ADDR + ":"  + MONGODB_PORT_27017_TCP_PORT + "/" + "docker")			
+		}else{
+			Session, _ = mgo.Dial("localhost")
+		}
+	}
+
+//	mongodb://myuser:mypass@localhost:40001/mydb	
 
 	Session.SetMode(mgo.Monotonic, true)		
-	Collection = Session.DB(MONGODB_INSTANCE_NAME).C("task")
+	
+	if MONGODB_INSTANCE_NAME != "" {
+		Collection = Session.DB(MONGODB_INSTANCE_NAME).C("task")
+	} else {
+		Collection = Session.DB("docker").C("task")
+	}
 	
 	return Collection
 }
