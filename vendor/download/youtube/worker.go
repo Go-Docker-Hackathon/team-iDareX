8d8adc2e6eae808e9fe1ce72142354c58829c7e2
langshuang <409909041@qq.com>
2015-06-06 16:57:42 +0800

    修改七牛的源地址

--- vendor/download/youtube/worker.go
+++ vendor/download/youtube/worker.go
@@ -2,27 +2,27 @@ package youtube
 
 import (
 	"fmt"
-	"github.com/Go-Docker-Hackathon/team-iDareX/vendor/upload/qiniu"
 	"github.com/Go-Docker-Hackathon/team-iDareX/vendor/db/mongo"
+	"github.com/Go-Docker-Hackathon/team-iDareX/vendor/upload/qiniu"
 	"labix.org/v2/mgo/bson"
 )
 
-type Worker struct{
-	ID int
-	Work chan WorkRequest
+type Worker struct {
+	ID          int
+	Work        chan WorkRequest
 	WorkerQueue chan chan WorkRequest
-	QuitChan chan bool
+	QuitChan    chan bool
 }
 
 func NewWroker(id int, workerQueue chan chan WorkRequest) Worker {
 	// Create, and return the worker.
-	
+
 	worker := Worker{
-		ID: id,
-		Work: make(chan WorkRequest),
+		ID:          id,
+		Work:        make(chan WorkRequest),
 		WorkerQueue: workerQueue,
-		QuitChan: make(chan bool)}
-	
+		QuitChan:    make(chan bool)}
+
 	return worker
 }
 
@@ -31,24 +31,24 @@ func (w Worker) Start() {
 		for {
 			// Add ourselves into the worker queue.
 			w.WorkerQueue <- w.Work
-			
+
 			select {
 			case work := <-w.Work:
 				// Receive a work request.
 				fmt.Printf("worker%d: Received work request\n", w.ID)
 				fmt.Printf("worker %d: Url: %s\n", w.ID, work.Url)
-				
+
 				fileName, err := YoutubeDl(work.Url)
 				if err != nil {
 					fmt.Println("error with YoutubeDl:", err)
 				}
 				fmt.Println("filename:", fileName, "on worker")
-				
+
 				// change task status
 				C := mongo.Connect()
 				C.Update(bson.M{"fetchurl": work.Url}, bson.M{"$set": bson.M{"status": 2}}) // downloading
 				fmt.Println("mongodb downloading")
-				
+
 				fmt.Println("upload to qiniu: ", fileName)
 				C.Update(bson.M{"fetchurl": work.Url}, bson.M{"$set": bson.M{"status": 3}}) // start upload
 				fmt.Println("mongodb start upload")
@@ -56,15 +56,15 @@ func (w Worker) Start() {
 				key, err1 := qiniu.UploadQiniu(fileName)
 				if err1 != nil {
 					fmt.Println("upload file to qiniu error:", err1, "filename:", fileName)
-				}else{
+				} else {
 					C.Update(bson.M{"fetchurl": videoUrl}, bson.M{"$set": bson.M{"status": 4}}) // upload finish
 					fmt.Println("mongodb upload finish")
-					downloadurl := "http://7xjhxh.com1.z0.glb.clouddn.com" + key
+					downloadurl := qiniu.DOMAIN + key
 					C.Update(bson.M{"fetchurl": work.Url}, bson.M{"$set": bson.M{"downloadurl": downloadurl}}) // set download key
 					fmt.Println("mongodb set download key")
 					fmt.Println("upload success, key:", key)
 				}
-				
+
 			case <-w.QuitChan:
 				// We have been asked to stop.
 				fmt.Printf("worker %d stopping\n", w.ID)
@@ -74,7 +74,7 @@ func (w Worker) Start() {
 }
 
 // Stop tells the worker to stop listening for work requests.
-// 
+//
 // Note that the worker will only stop *after* it has finished its work.
 func (w Worker) Stop() {
 	go func() {
