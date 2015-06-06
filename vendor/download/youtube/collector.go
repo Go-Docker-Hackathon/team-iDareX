c4dcfbf7afcb6e8082db87eb24c4bdb63c23ad7e
maxwelldu <dcj3sjt@163.com>
2015-06-06 16:16:03 +0800

    add print when mongodb change status

--- vendor/download/youtube/collector.go
+++ vendor/download/youtube/collector.go
@@ -3,6 +3,7 @@ package youtube
 import (
 	"github.com/Go-Docker-Hackathon/team-iDareX/vendor/db/mongo"
 	"labix.org/v2/mgo/bson"
+	"fmt"
 )
 
 var WorkQueue = make(chan WorkRequest, 100)
@@ -10,10 +11,13 @@ var WorkQueue = make(chan WorkRequest, 100)
 func Collector(url string) {
 	C := mongo.Connect()
 	C.Update(bson.M{"fetchurl": url}, bson.M{"$set": bson.M{"status": 1}}) // start download
+	fmt.Println("mongodb start download")
 	
 	work := WorkRequest{ Url: url}	
 	WorkQueue <- work
 	
 	// change task status
 	C.Update(bson.M{"fetchurl": url}, bson.M{"$set": bson.M{"status": 2}}) // downloading
+	fmt.Println("mongodb downloading")
+
 }
